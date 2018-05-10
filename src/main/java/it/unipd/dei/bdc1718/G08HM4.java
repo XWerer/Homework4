d247e2135ce75ddda39dd14a8b7a47bf89a52c6e
XWerer <dadomw2@gmail.com>
2018-05-10 10:00:43 +0200

    Aggiunto solo l'aquisizione dei parametri e basta per ora

--- src/main/java/it/unipd/dei/bdc1718/G08HM4.java
+++ src/main/java/it/unipd/dei/bdc1718/G08HM4.java
@@ -1,7 +1,10 @@
 package it.unipd.dei.bdc1718;
 
 /*
-* HOMEWORK 4
+* HOMEWORK 4 BIG DATA 2017/18
+* Davide Masiero
+* Riccardo Castelletto
+* Teo Spadotto
 * */
 
 import org.apache.spark.SparkConf;
@@ -16,6 +19,20 @@ import java.util.Scanner;
 public class G08HM4 {
 
   public static void main(String[] args) throws FileNotFoundException {
+    if (args.length != 3)
+      throw new IllegalArgumentException("Expecting 3 parameters (file name, k, numBlocks)");
+
+    //Some variables
+    int k, numBlocks;
+
+    //Take the value k and numBlocks form the command line
+    try {
+      k = Integer.valueOf(args[1]);
+      numBlocks = Integer.valueOf(args[2]);
+    }
+    catch (Exception e) {
+      throw new IllegalArgumentException("k and numBlocks must be integer");
+    }
 
   }
 }
