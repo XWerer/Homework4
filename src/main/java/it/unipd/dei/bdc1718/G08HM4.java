fc948fddfce2431e609700964168005cc5d3901d
XWerer <dadomw2@gmail.com>
2018-05-10 09:51:46 +0200

    Initial commit

---
+++ src/main/java/it/unipd/dei/bdc1718/G08HM4.java
@@ -0,0 +1,21 @@
+package it.unipd.dei.bdc1718;
+
+/*
+* HOMEWORK 4
+* */
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class G08HM4 {
+
+  public static void main(String[] args) throws FileNotFoundException {
+
+  }
+}
